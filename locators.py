7af762445d24e64c652d95061f8913a6d460e10b
evin71 <64774022+evin71@users.noreply.github.com>
2021-11-22 23:35:40 +0530

    Update locators.py

--- locators.py
+++ locators.py
@@ -5,12 +5,9 @@ class MainPageLocators(object):
 	password = (By.NAME, 'password')
 	login = (By.ID, "loginForm")
 	
+	# Popups locators
 	notification_turn_off = (By.XPATH, '/html/body/div[4]/div/div/div/div[3]/button[2]')
-	not_now = (By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')
-
-	search = (By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-	first_one_search_list = (By.XPATH, '//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a/div')
-
+	save_login_info_not_now = (By.XPATH, '//*[@id="react-root"]/section/main/div/div/div/div/button')
 
 class UserLocators(object):
 	name = (By.CLASS_NAME, 'rhpdm')
@@ -21,19 +18,9 @@ class UserLocators(object):
 	# class name of h2 tag which display "This Account is Private"
 	isprivate = (By.CLASS_NAME, 'rkEop')
 
-	# class name of li of followers
+	# class name of li of followers and following
 	li_follow_cls = (By.CLASS_NAME, 'wo9IH')
 
 	# used in js script
 	# class name of the div containg the list of followers and following
 	div_cls = 'isgrP'
-
-
-class Message(object):
-	pass
-
-		
-
-
-
-
